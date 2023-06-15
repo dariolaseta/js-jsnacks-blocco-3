5795f96b3f0b7bdb18dc30d06fc091c33be20203
dariolaseta <dario.laseta@gmail.com>
2023-06-15 12:47:36 +0200

    added li without style

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Cibo per gatti</a>
+                </li>
+                <li>
+                    <a href="#">Cibo per cani</a>
+                </li>
+                <li>
+                    <a href="#">Cibo per Pokémon</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
